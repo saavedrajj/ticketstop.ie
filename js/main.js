57cf9dc81fddfe32a06ec91b0d4d400c212fe0b2
Saavedra <jusaavedra@paypalcorp.com>
2017-04-26 08:08:11 +0100

    w

--- js/main.js
+++ js/main.js
@@ -1,5 +1,45 @@
-$('#nav').affix({
-      offset: {
-        top: $('header').height()
-      }
-}); 
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+});
+
+(function($, document, window, viewport){
+
+    var highlightBox = function( className ) {
+        $(className).addClass('active');
+    }
+
+    var highlightBoxes = function() {
+
+
+     if( viewport.is("xs") ) {
+
+    }
+
+    if( viewport.is("sm") ) {
+
+    }
+
+
+    if( viewport.is("md") ) {
+
+    }
+
+    if( viewport.is("lg") ) {
+
+    }
+}
+
+    // Executes once whole document has been loaded
+    $(document).ready(function() {
+        highlightBoxes();
+        console.log('Current breakpoint:', viewport.current());
+    });
+
+    $(window).resize(
+        viewport.changed(function(){
+          highlightBoxes();
+         console.log('Current breakpoint:', viewport.current());
+     })
+        );
+
+})(jQuery, document, window, ResponsiveBootstrapToolkit);
